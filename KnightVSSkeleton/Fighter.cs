fa508a64cccdc8a8ee18a4356068faba945e7cbd
Unknown <9771394@gmail.com>
2017-01-28 11:35:23 +0300

    Исходный код

---
+++ KnightVSSkeleton/Fighter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KnightVSSkeleton
+{
+    public class Fighter
+    {
+       private int health;
+       PictureBox sprite;
+       public Fighter (PictureBox sprite)
+        {
+       this.sprite = sprite;
+       this.health = 100;
+        }
+        public int MakeDamage() 
+        {
+        Random random = new Random();
+        return random.Next(1, 10);
+        }
+        
+        public void ReceiveDamage(int howMuchDamage)
+        {
+        health -= howMuchDamage;
+        if (health <= 0)
+            {
+            health = 0;
+                Die();
+            }
+        }
+        public int TellHealth()
+        {
+            return health;
+        }
+        
+        private async void Die()
+        {
+            sprite.Image = Image.FromFile(@"E:\Anton Hacker\KnightVSSkeleton-master\KnightVSSkeleton-master\Assets\Skeleton_Death.gif");
+            await Task.Delay(1000);
+            sprite.Enabled = false;
+        }
+
+        public bool IsDead()
+        {
+            if (health <= 0) return true;
+            else return false;
+        }
+      
+         
+ 
+        }
+
+    
+    
+      
+        
+    }
+
+
+ 
